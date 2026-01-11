802155328a7bee694d4e85384ba78d87a2c54917
kwonkeonhyeong <rjsgud19566@gmail.com>
2026-01-11 23:35:35 +0900

    feat: 초기 프로젝트 설정 및 Gradle 구성 추가

---
+++ cic/src/main/java/cic/CicApplication.java
@@ -0,0 +1,13 @@
+package cic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class CicApplication {
+    @SpringBootApplication
+    public class MomentApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(CicApplication.class, args);
+        }
+    }
+}
