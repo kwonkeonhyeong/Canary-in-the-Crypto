a6be40744c5f6016312b15580ee1b20178af8159
kwonkeonhyeong <rjsgud19566@gmail.com>
2026-01-11 22:02:27 +0900

    feat: BinanceAggTradeDto 클래스 추가

---
+++ cic/src/main/java/cic/dto/BinanceAggTradeDto.java
@@ -0,0 +1,44 @@
+package cic.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * Binance Aggregated Trade 데이터 매핑
+ *
+ * Payload 예시:
+ * {
+ *   "e": "aggTrade",  // Event type
+ *   "E": 1672515782,  // Event time (eventTime)
+ *   "s": "BTCUSDT",   // Symbol (symbol)
+ *   "a": 12345,       // Aggregate trade ID
+ *   "p": "0.001",     // Price (price)
+ *   "q": "100",       // Quantity (quantity)
+ *   "f": 100,         // First trade ID
+ *   "l": 105,         // Last trade ID
+ *   "T": 1672515781,  // Trade time
+ *   "m": true,        // Is the buyer the market maker? (isBuyerMaker)
+ *   "M": true         // Ignore
+ * }
+ */
+@Getter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BinanceAggTradeDto {
+    @JsonProperty("s")
+    private String symbol;
+
+    @JsonProperty("p")
+    private String price;
+
+    @JsonProperty("q")
+    private String quantity;
+
+    @JsonProperty("E")
+    private Long eventTime;
+
+    @JsonProperty("m")
+    private Boolean isBuyerMaker;
+}
